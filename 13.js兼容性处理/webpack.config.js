68ff643c5c53243d7c32d7a2025358de37c4c67b
lijunpeng <879599011@qq.com>
2022-01-14 18:26:15 +0800

    13.js兼容性处理

---
+++ 13.js兼容性处理/webpack.config.js
@@ -0,0 +1,47 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/built.js',
+    path: resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      /*
+          js兼容处理：babel-loader @babel/core @babel/preset-env
+            1.基本js兼容性处理 --> @babel/preset-env
+              问题：只能转换基本语法，如promise高级语法不能转换
+            2.全部js兼容性处理 --> @babel/poltfill
+              问题：需求只要解决部分兼容性问题，但是将所有兼容性代码全部引入，体积太大了~
+            3.按需加载 --> core-js
+        */
+      {
+        test: /\.js$/,
+        // 排除node_modules
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        // 预设：指示bable做怎么样的兼容性处理
+        options: {
+          presets: [['@babel/preset-env', {
+            targets: {
+              edge: '17',
+              firefox: '60',
+              chrome: '67',
+              safari: '11.1',
+              ie: '11',
+            },
+            useBuiltIns: 'usage',
+          }]],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  mode: 'development',
+};
