dd1c30714cc943888138d345c3dbb70ad7f98dbb
lijunpeng <879599011@qq.com>
2022-01-14 11:07:09 +0800

    12.js语法检查

--- 12.js语法检查/src/js/index.js
+++ 12.js语法检查/src/js/index.js
@@ -1,6 +1,6 @@
-
-function add(x,y){
-    return x + y
+function add(x, y) {
+  return x + y;
 }
-
-console.log(add(1,2))
+// 下一行不eslint所有规则都失效（下一行不进行eslint检测）
+// eslint-disable-next-line
+console.log(add(1, 2));
--- 12.js语法检查/webpack.config.js
+++ 12.js语法检查/webpack.config.js
@@ -1,14 +1,17 @@
 const { resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: './src/js/index.js',
-    output: {
-        filename: "js/built.js",
-        path: resolve(__dirname,"build")
-    },
-    module: {
-        rules: [
-            /*
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/built.js',
+    path: resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      /*
+                !!!!!!!版本    "eslint-loader": "^4.0.2",      "eslint": "^7.32.0",
+
                 语法检查：eslint-loader eslint
                     注意：只检查自己写的源代码，第三方的库是不用检查的
                     设置检查规则：
@@ -18,22 +21,31 @@ module.exports = {
                       }
                     airbnb --> eslint-config-airbnb-base eslint-plugin-import eslint
                     下载 npm i eslint-loader eslint eslint-config-airbnb-base eslint-plugin-import -D
+
+                    黄色警告类似：
+                    5:1  warning  Unexpected console statement  no-console
+                    ✖ 1 problem (0 errors, 1 warning)
+                    可以在package.json的 "eslintConfig": {}里加 关闭console校验
+                    "rules": {
+                      "no-console":"off"
+                    } 进行关闭
             */
-            {
-                test: /\.js$/,
-                // 排除node_modules
-                exclude: /node_modules/,
-                loader: "eslint-loader",
-                options: {
-                    fix: true,
-                },
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
+      {
+        test: /\.js$/,
+        // 排除node_modules
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          // 自动修复eslint的错误
+          fix: true,
+        },
+      },
     ],
-    mode: "development"
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  mode: 'development',
+};
