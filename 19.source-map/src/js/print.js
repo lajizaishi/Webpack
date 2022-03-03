dd84032eff0dae4b35cfc788ae43e3449e436d50
lijunpeng <879599011@qq.com>
2022-03-03 10:46:38 +0800

    19.source-map

--- 19.source-map/src/js/print.js
+++ 19.source-map/src/js/print.js
@@ -1,6 +1,6 @@
 console.log('print.js被加载了~');
 const print = function print() {
   const content = 'hello print webpack';
-  console.log(content);
+  console.log(content)();
 };
 export default print;
