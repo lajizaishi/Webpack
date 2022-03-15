a18d4c3fa679044a5e739ec53101c0fc2eeaded0
lajizaishi <879599011@qq.com>
2022-03-15 23:15:47 +0800

    23.code_split

---
+++ 23.code_split/demo2/src/js/test.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/no-unresolved,import/no-extraneous-dependencies
+import $ from 'jquery';
+
+console.log($);
+export function mul(x, y) {
+  return x + y;
+}
+
+export function count(x, y) {
+  return x - y;
+}
